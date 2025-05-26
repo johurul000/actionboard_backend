cd986dc8ac93237df6f199585f86fb29141169e3
Johurul Hassan Mazumder <johurulhassan000@gmail.com>
2025-05-26 06:21:01 +0530

    increased the JWT access token time for testing APIs

--- actionboard_back/settings.py
+++ actionboard_back/settings.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_TOKEN_CLASSES': (
