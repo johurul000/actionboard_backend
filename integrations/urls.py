92b975b9101b83f715551059dbaf7403ef039a10
Azizur rahman (Ayan) <45368651+ayanchyaziz123@users.noreply.github.com>
2025-06-01 19:08:51 -0400

    Update urls.py

--- integrations/urls.py
+++ integrations/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('zoom/oauth/start/', ZoomOAuthStartView.as_view()),
     path('zoom/oauth/callback/', ZoomOAuthCallbackView.as_view()),
+     path('zoom/status/', ZoomConnectionStatusView.as_view(), name='zoom-status'), 
 ]
