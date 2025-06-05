7812854a76cbc35d4c2c6712bafcd4ae4176f35c
Johurul Hassan Mazumder <johurulhassan000@gmail.com>
2025-06-06 05:18:20 +0530

    Updated the integration views

--- integrations/admin.py
+++ integrations/admin.py
@@ -3,7 +3,7 @@ from .models import OAuthToken, ZoomProfile
 
 @admin.register(OAuthToken)
 class OAuthTokenAdmin(admin.ModelAdmin):
-    list_display = ('organisation', 'provider', 'expires_at', 'created_at')
+    list_display = ('user', 'organisation', 'provider', 'expires_at', 'created_at')
     list_filter = ('provider', 'organisation__name')
     search_fields = ('organisation__name', 'provider')
     readonly_fields = ('created_at',)
@@ -11,7 +11,7 @@ class OAuthTokenAdmin(admin.ModelAdmin):
 
 @admin.register(ZoomProfile)
 class ZoomProfileAdmin(admin.ModelAdmin):
-    list_display = ('zoom_email', 'organisation', 'zoom_user_id', 'zoom_account_id', 'last_synced_at')
+    list_display = ('user', 'zoom_email', 'organisation', 'zoom_user_id', 'zoom_account_id', 'last_synced_at')
     search_fields = ('zoom_email', 'zoom_user_id', 'organisation__name')
     list_filter = ('organisation__name',)
     readonly_fields = ('created_at', 'updated_at', 'last_synced_at')
